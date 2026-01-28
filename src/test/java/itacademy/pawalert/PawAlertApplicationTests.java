32576f81f8d034012afca9ceb9c35d4ac03f1027
Esther <githubesther@gmail.com>
2026-01-28 13:46:06 +0100

    Initial commit

---
+++ src/test/java/itacademy/pawalert/PawAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package itacademy.pawalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawAlertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
