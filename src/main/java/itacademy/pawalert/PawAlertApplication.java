b57e0931969c0d2bf2b51f3d2826620fcc50a031
Esther <githubesther@gmail.com>
2026-01-28 13:46:06 +0100

    Initial commit

---
+++ src/main/java/itacademy/pawalert/PawAlertApplication.java
@@ -0,0 +1,13 @@
+package itacademy.pawalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawAlertApplication.class, args);
+    }
+
+}
