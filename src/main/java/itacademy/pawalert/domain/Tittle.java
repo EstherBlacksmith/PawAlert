aeeb3a2ebd3e8430edf42cbc234c85d26e668a6a
Esther <githubesther@gmail.com>
2026-01-29 13:53:01 +0100

    refactor(domain): update Description and Title classes implementation

---
+++ src/main/java/itacademy/pawalert/domain/Tittle.java
@@ -0,0 +1,10 @@
+package itacademy.pawalert.domain;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record Tittle(@NotBlank(message = "The Tittle can not be empty")
+                     @Min(value = 5, message = "The tittle must be almost 5 characters")
+                     @Max(value = 255,message = "The tittle must be less than 50 characters") String Tittle) {
+}
