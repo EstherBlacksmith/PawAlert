ee6f9428eacb9f3167fa8e278e6a52bcf0c36353
Esther <githubesther@gmail.com>
2026-01-29 13:53:01 +0100

    refactor(domain): update Description and Title classes implementation

---
+++ src/main/java/itacademy/pawalert/domain/Description.java
@@ -0,0 +1,11 @@
+package itacademy.pawalert.domain;
+
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record Description(@NotBlank(message = "Description can not be empty")
+                          @Min(value = 5, message = "The description must be almost 5 characters")
+                          @Max(value = 255,message = "The description must be less than 255 characters") String description) {
+}
