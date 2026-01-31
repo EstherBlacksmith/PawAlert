170ce63c49edd2b98ecefb83dbacfb0fb2b654b8
Esther <githubEsther@gmail.com>
2026-01-31 16:46:58 +0100

    refactor(repository): change AlertRepository ID type from Long to String

--- src/main/java/itacademy/pawalert/infrastructure/persistence/AlertRepository.java
+++ src/main/java/itacademy/pawalert/infrastructure/persistence/AlertRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AlertRepository extends JpaRepository<AlertEntity, Long> {
+public interface AlertRepository extends JpaRepository<AlertEntity, String> {
     List<AlertEntity> findByStatus(String status);
-    List <AlertEntity> findAllByPetId(Long petID);
+    List <AlertEntity> findAllByPetId(String petID);
 }
