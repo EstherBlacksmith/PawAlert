2cfb9becb52acd6971e68d4ce5f8c405241bd53d
Esther <githubEsther@gmail.com>
2026-01-31 16:45:47 +0100

    feat(domain): add getValue() method to domain records

--- src/main/java/itacademy/pawalert/domain/Tittle.java
+++ src/main/java/itacademy/pawalert/domain/Tittle.java
@@ -6,5 +6,8 @@ import jakarta.validation.constraints.NotBlank;
 
 public record Tittle(@NotBlank(message = "The Tittle can not be empty")
                      @Min(value = 5, message = "The tittle must be almost 5 characters")
-                     @Max(value = 255,message = "The tittle must be less than 50 characters") String Tittle) {
+                     @Max(value = 255,message = "The tittle must be less than 50 characters") String tittle) {
+    public String getValue() {
+        return tittle;
+    }
 }
