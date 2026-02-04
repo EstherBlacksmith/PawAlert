34580fdc7c6980847b8b8654397739d10b3eec03
Esther <githubEsther@gmail.com>
2026-02-04 19:59:11 +0100

    chore(alert): remove unused StatusNames import from CreateAlertUseCase

--- src/main/java/itacademy/pawalert/application/port/inbound/CreateAlertUseCase.java
+++ src/main/java/itacademy/pawalert/application/port/inbound/CreateAlertUseCase.java
@@ -1,6 +1,5 @@
 package itacademy.pawalert.application.port.inbound;
 import itacademy.pawalert.domain.alert.model.Alert;
-import itacademy.pawalert.domain.alert.model.StatusNames;
 
 public interface CreateAlertUseCase {
     Alert createOpenedAlert(String petId, String title, String description, String userId);
