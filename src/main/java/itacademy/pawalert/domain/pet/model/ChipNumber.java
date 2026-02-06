ff355311b7e3850e59f72412e4e61262ddb5b906
Esther <githubEsther@gmail.com>
2026-02-06 21:36:36 +0100

    refactor(pet): refactor ChipNumber validation to use explicit constructor logic

--- src/main/java/itacademy/pawalert/domain/pet/model/ChipNumber.java
+++ src/main/java/itacademy/pawalert/domain/pet/model/ChipNumber.java
@@ -1,23 +1,36 @@
 package itacademy.pawalert.domain.pet.model;
 
-import itacademy.pawalert.domain.user.model.Email;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
-public record ChipNumber(@Size(min = 15, max = 15, message = "The chip must be exactly 15 characters long")
-                         @Pattern(regexp = "^[0-9]*$", message = "The chip must be only made up of numeric characters")
-                         @ValidCountryChipCode(message = "Invalid country chip code") String chip) {
+public record ChipNumber(@ValidCountryChipCode(message = "Invalid country chip code") String value) {
+
+    private static final int MIN_LENGTH = 9;
+    private static final int MAX_LENGTH = 15;
+    public ChipNumber {
+
+        value = value.trim();
+
+        if (!value.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("The chip must be only made up of numeric characters");
+        }
+
+        if (value.length() < MIN_LENGTH || value.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(
+                    "The chip must be exactly 15 characters long");
+        }
+    }
 
     public static ChipNumber empty() {
         return new ChipNumber(null);
     }
 
     public boolean isPresent() {
-        return chip != null;
+        return value != null;
     }
 
-    public String value() { return this.chip; }
-    public static ChipNumber of(String chip) {
-        return new ChipNumber(chip);
+    public String value() {
+        return this.value; }
+
+    public static ChipNumber of(String value) {
+        return new ChipNumber(value);
     }
 }
