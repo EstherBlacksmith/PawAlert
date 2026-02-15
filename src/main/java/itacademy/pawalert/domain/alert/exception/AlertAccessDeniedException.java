d826d6afcf410eb44bdde29e6b5359c13fb55544
Esther <githubEsther@gmail.com>
2026-02-15 20:28:09 +0100

    feat: New AlertAccessDeniedException and ForbiddenException for provide a specific error exception

---
+++ src/main/java/itacademy/pawalert/domain/alert/exception/AlertAccessDeniedException.java
@@ -0,0 +1,16 @@
+package itacademy.pawalert.domain.alert.exception;
+
+import java.util.UUID;
+
+
+public class AlertAccessDeniedException extends RuntimeException {
+    
+    public AlertAccessDeniedException(UUID alertId, UUID userId) {
+        super(String.format("User %s is not authorized to modify alert %s. Only the alert owner or an administrator can perform this action.", 
+            userId, alertId));
+    }
+    
+    public AlertAccessDeniedException(String message) {
+        super(message);
+    }
+}
