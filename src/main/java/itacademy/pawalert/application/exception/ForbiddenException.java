c1f379fbb77c1d06960aafd84240a894c5908b8d
Esther <githubEsther@gmail.com>
2026-02-15 20:28:09 +0100

    feat: New AlertAccessDeniedException and ForbiddenException for provide a specific error exception

---
+++ src/main/java/itacademy/pawalert/application/exception/ForbiddenException.java
@@ -0,0 +1,14 @@
+package itacademy.pawalert.application.exception;
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+    public static ForbiddenException notAuthorized(String action) {
+        return new ForbiddenException(
+            "You are not authorized to " + action
+        );
+    }
+}
